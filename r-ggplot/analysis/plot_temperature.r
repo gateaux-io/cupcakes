9af47c8e71a8245c8dbd42ced33cff2b884d7bd4
Edward Abraham <edward@dragonfly.co.nz>
2023-02-28 11:45:50 +1300

    Make plot black and white

--- r-ggplot/analysis/plot_temperature.r
+++ r-ggplot/analysis/plot_temperature.r
@@ -3,7 +3,8 @@ library(ggplot2)
 anomaly <- read.csv('../data/global_temperature_anomaly.csv')
 g <- ggplot(anomaly, aes(x=year, y=january_c, fill= (january_c < 0))) + 
     geom_bar(stat='identity') +
-    xlab('Year')
+    xlab('Year') + 
+    theme_bw()
 
 ggsave(g, width=9, height=6, file='/output/global_temperature.pdf')
 
