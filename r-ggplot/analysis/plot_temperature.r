e64a2081cb572cc4f774a02ddc59f2da9f0535e9
Edward Abraham <edward@dragonfly.co.nz>
2023-02-28 11:09:52 +1300

    initial plot

---
+++ r-ggplot/analysis/plot_temperature.r
@@ -0,0 +1,8 @@
+library(ggplot2)
+
+anomaly <- read.csv('../data/global_temperature_anomaly.csv')
+g <- ggplot(anomaly, aes(x=year, y=january_c, fill= (january_c < 0))) + 
+    geom_bar(stat='identity')
+
+ggsave(g, width=9, height=6, file='/output/global_temperature.pdf')
+
